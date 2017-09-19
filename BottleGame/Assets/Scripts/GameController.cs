21dec09194439fd7132661a32c406af399fd8e4b
AndrewKarasev <anvkarasev@gmail.com>
2017-09-19 18:44:05 +0300

    Добавил положительную анимацию

--- BottleGame/Assets/Scripts/GameController.cs
+++ BottleGame/Assets/Scripts/GameController.cs
@@ -1,8 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameController : MonoBehaviour {
+
+	public GameObject Bottle;
+
+	public void RotateBottle ()
+	{
+		Bottle.GetComponent<Animation> ().Play ("+315");
+	}
+
 
 	// Use this for initialization
 	void Start () {
