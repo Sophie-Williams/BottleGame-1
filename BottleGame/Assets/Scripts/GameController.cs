7d8d8e15560e72c4220de1052b9ea3d847d43484
AndrewKarasev <anvkarasev@gmail.com>
2017-09-21 18:53:20 +0300

    Убрал текстуру. Вместо списков сделал массивы. Добавил различные разрешения экранов для смартфонов.

--- BottleGame/Assets/Scripts/GameController.cs
+++ BottleGame/Assets/Scripts/GameController.cs
@@ -5,15 +5,17 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour {
 
 	public GameObject Bottle;
-	public List <AnimationClip> anim =new List <AnimationClip>();
-	public List <string> wishString = new List <string> ();
+	public AnimationClip[] anim = new AnimationClip[16];
+	//public List <string> wishString = new List <string> ();
 	public Text wishTextComponent;
+	public string[] wishString = new string[100];
+
 
 	public void StartAnimation ()
 	{
-		int randAnim = Random.Range (0, anim.Count);
-		int randString = Random.Range (0, wishString.Count);
-		Debug.Log (randString);
+		int randAnim = Random.Range (0, 16);
+		int randString = Random.Range (0, 3);
+		Debug.Log (randAnim);
 		Bottle.GetComponent<Animation> ().Play (anim[randAnim].name);
 		wishTextComponent.text = wishString [randString];
 	}
