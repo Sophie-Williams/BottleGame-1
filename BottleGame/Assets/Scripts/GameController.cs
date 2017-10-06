55c5b9cf1809dc4c71baef10c8f230191c0dbcc9
AndrewKarasev <anvkarasev@gmail.com>
2017-10-06 19:33:45 +0300

    Добавил желаний. Подготовка к выгрузке

--- BottleGame/Assets/Scripts/GameController.cs
+++ BottleGame/Assets/Scripts/GameController.cs
@@ -6,20 +6,18 @@ public class GameController : MonoBehaviour {
 
 	public GameObject Bottle;
 	public AnimationClip[] anim = new AnimationClip[16];
-	//public List <string> wishString = new List <string> ();
+
 	public Text wishTextComponent;
 	public string[] wishString = new string[100];
 	private int countOfPress;
-	public Text countPress;
 	private int randString;
 
 	public void StartAnimation ()
 	{
 		int randAnim = Random.Range (0, 16);
-		randString = Random.Range (0, 50);
+		randString = Random.Range (0, 55);
 		Debug.Log (randString);
 		countOfPress++;
-		countPress.text = countOfPress.ToString();
 		Bottle.GetComponent<Animation> ().Play (anim[randAnim].name);
 		wishTextComponent.text = wishString [randString];
 	}
